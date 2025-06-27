8c1e9f557ff92885f4317ead05cca4ebfc1d6558
gelangn <garay.n.a@gmail.com>
2025-06-27 11:08:51 +0200

    Agregar función ft_isfloat para validar números en punto flotante; actualizar Makefile para incluir el nuevo archivo fuente.

---
+++ lib/libft/ft_isfloat.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_isfloat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anavas-g <anavas-g@student.42urduliz.co    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/27 11:05:22 by anavas-g          #+#    #+#             */
+/*   Updated: 2025/06/27 11:08:09 by anavas-g         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_isfloat(const char *str)
+{
+	int has_dot;
+
+	has_dot = 0;
+	if (*str == '-' || *str == '+')
+		str++;
+	while (*str)
+	{
+		if (*str == '.')
+		{
+			if (has_dot)
+				return (0);
+			has_dot = 1;
+		}
+		else if (!ft_isdigit(*str))
+			return (0);
+		str++;
+	}
+	return (1);
+}
